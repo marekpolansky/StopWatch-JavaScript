6533397660e2272d40fe0c13d59d5dbbf128de67
Peter Skok - VSC <ptskokmine@gmail.com>
2023-01-04 17:03:20 +0100

    chanes in backend js

--- Index.js
+++ Index.js
@@ -1,2 +1,22 @@
-let name = 'Peter';
-console.log(name);
+let [milliseconds,seconds,minutes,hours] = [0,0,0,0];
+let timerRef = document.querySelector('.timerDisplay');
+let startBtn = document.getElementById('start');
+let int = null;
+startBtn.addEventListener('click', ()=>
+    {
+        if(int!==null){
+            clearInterval(int);
+        }
+        int = setInterval(displayTimer,10);
+    }
+);
+
+function displayTimer(){
+    milliseconds += 10;
+    if(milliseconds == 1000){
+        seconds += 1;
+        milliseconds = 0;
+    }
+    console.log(seconds);
+    timerRef.innerText = seconds;
+}
