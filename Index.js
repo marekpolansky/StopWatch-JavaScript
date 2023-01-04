47c6063c985c2e110014fac5b0ce96001e64f380
Marek Polasnky <marek.polansky@student.tuke.sk>
2023-01-04 17:38:53 +0100

    m

--- Index.js
+++ Index.js
@@ -2,6 +2,11 @@ let [milliseconds,seconds,minutes,hours] = [0,0,0,0];
 let timerRef = document.querySelector('.timerDisplay');
 let startBtn = document.getElementById('start');
 let int = null;
+let pomocna = '0';
+let pomocnami = '0';
+let pomocnam = '0';
+let pomocnah = '0';
+
 startBtn.addEventListener('click', ()=>
     {
         if(int!==null){
@@ -13,10 +18,46 @@ startBtn.addEventListener('click', ()=>
 
 function displayTimer(){
     milliseconds += 10;
+    
     if(milliseconds == 1000){
         seconds += 1;
         milliseconds = 0;
     }
-    console.log(seconds);
-    timerRef.innerText = seconds;
+    if (seconds == 60){
+        minutes += 1;
+        seconds = 0;
+    }
+    if (minutes == 60){
+        hours += 1;
+        minutes = 0;
+    }
+    
+    if (seconds > 9){
+        pomocna = '';
+    }
+    if (minutes > 9){
+        pomocnam = '';
+    }
+    if (hours > 9){
+        pomocnah = '';
+    }
+    if (milliseconds > 9){
+        pomocnami = '';
+    }
+    // if (milliseconds == 0){
+    //     pomocnami = '0';
+    // }
+    if (seconds == 0){
+        pomocna = '0';
+    }
+    if (minutes == 0){
+        pomocnam = '0';
+    }
+    if (hours == 0){
+        pomocnah = '0';
+    }
+    console.log(pomocnami);
+
+    
+    timerRef.innerText = pomocnah + hours + ' : ' + pomocnam + minutes + ' : ' + pomocna + seconds + ' : ' + pomocnami + milliseconds / 10;
 }
